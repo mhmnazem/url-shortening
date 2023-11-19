471a0725926c15d528f85a30ec825bb7dfa74eaf
Mohammad Nazemzadeh <mohammad.nazem@gmail.com>
2023-11-20 00:00:56 +0100

    Initial commit

---
+++ src/test/kotlin/com/kofa/urlshortening/UrlShorteningApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kofa.urlshortening
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlShorteningApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
